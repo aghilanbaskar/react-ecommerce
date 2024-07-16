c8a86fff4331f96f854833b3700ca8a8841c2e32
Aghilan Baskar <aghilanbaskar@gmail.com>
2024-07-16 12:36:33 +0530

    eslint

--- src/components/forms/Buttons.tsx
+++ src/components/forms/Buttons.tsx
@@ -1,4 +1,4 @@
-import{ FC, ButtonHTMLAttributes, ReactNode } from 'react';
+import { FC, ButtonHTMLAttributes, ReactNode } from 'react';
 
 interface FormButtonsProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
